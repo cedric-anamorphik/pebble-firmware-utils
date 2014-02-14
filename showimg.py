2f7ebb2fa685473fd76cb87bc15b4f30eea14fd7
Semyon Maryasin <simeon@maryasin.name>
2014-02-14 20:23:35 +0400

    showimg: support images from tintin_fw binary

--- showimg.py
+++ showimg.py
@@ -23,21 +23,37 @@ def showImage(data, rowsize, width, height, inverse=False):
 def calcHeightBySize(rowsize, datasize):
     return datasize/rowsize
 
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description = "This script displays images found in Pebble firmware or resource files")
+    parser.add_argument("input", nargs='?', default="tintin_fw.bin", type=argparse.FileType("rb"),
+                        help="Input file, defaults to tintin_fw.bin")
+    parser.add_argument("-o", "--offset", type=lambda(n): int(n, 0),
+                        help="Offset to beginning of image. If omitted, image will be treated as resource-type file")
+    parser.add_argument("-i", "--invert", action="store_true",
+                        help="Inverse display (white/black)")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    f = sys.stdin
-    if len(sys.argv) >= 1 and sys.argv[0] in ['-h', '--help']:
-        print "Usage: cat image | showimg.py [-r] [offset]"
-        print "Possible example: dd if=tintin_fw.bin bs=1 count=64 skip=`wcalc 0x48af4 | tr -d '= '` | ../../showimg.py 4 16"
-        exit(1)
-    rev = len(sys.argv) >= 2 and sys.argv[1] == '-r'
-    shift = 0
-    if len(sys.argv) >= (3 if rev else 2):
-        shift = int(sys.argv[4 if rev else 3])
-    if shift:
-        f.read(shift)
-    rowsize = unpack('H', f.read(2))[0]
-    f.read(6) # two unknown fields
-    width = unpack('H', f.read(2))[0]
-    f.read(2) # height
-    data = sys.stdin.read()
-    showImage(data, rowsize, width, calcHeightBySize(rowsize, len(data)), rev)
+    args = parse_args()
+    f = args.input
+    if args.offset:
+        print args.offset
+        if args.offset > 0x08010000:
+            args.offset -= 0x08010000 # convert from memory address to file offset
+        f.read(args.offset)
+    s = f.read(4)
+    ofs = unpack('I', s)[0]
+    if 0x8010000 < ofs and ofs < 0x80fffff: # memory offset
+        rowsize = unpack('H', f.read(2))[0]
+        f.read(2) # unknown field = 0x1000
+    else: # resource file
+        ofs = None
+        rowsize = unpack('H', s[:2])[0]
+    f.read(4) # unknown field
+    width, height = unpack('HH', f.read(4))
+    if ofs:
+        f.seek(ofs-0x8010000)
+    data = f.read()
+    showImage(data, rowsize, width, height, args.invert) # or: calcHeightBySize(rowsize, len(data))
