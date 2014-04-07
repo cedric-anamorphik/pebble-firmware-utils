6f9cbb9b6b423b20e57f345b9d75ff6b0254c0bb
Semyon Maryasin <simeon@maryasin.name>
2014-04-07 20:53:06 +0400

    parser: refactor: moved instruction parser to separate proc
    This is needed to allow implementation of #ifdef's

--- libpatcher/parser.py
+++ libpatcher/parser.py
@@ -43,6 +43,96 @@ def uncomment(line):
         linewoc += c
     return linewoc.strip() # remove leading and trailing spaces
 
+def parseInstruction(line, pos):
+    """
+    This methods converts line from source file to Instruction (opcode and args?).
+    """
+    try:
+        opcode, arg = line.split(None,1)
+    except ValueError: # only one token
+        opcode = line
+        arg = ''
+
+    # now parse args
+    args = asm.List()
+    s = '' # string repr of current arg
+    t = None # type of current arg: None (no current), n(numeric), ',"(quoted str), l(reg or label)
+    br = False # whether we are in [] block
+    for c in arg+'\n': # \n will be processed as last character
+        domore = False # if current character needs to be processed further
+        if t == None: # state: no current arg
+            domore = True
+        elif t in "'\"": # quoted string
+            if c == t: # end of string
+                args.append(asm.Str(s))
+                s = ''
+                t = None
+            elif c == '\\': # backslash in string
+                t += '\\'
+            else:
+                s += c
+        elif t in ['"\\', "'\\"]: # state: backslash in quoted string
+            s += c
+            t=t[0]
+        elif t == 'n': # number, maybe hex
+            if c.isdigit() or c in 'xXbB':
+                s.append(c)
+            else:
+                domore = True # need to process current character further
+                args.append(asm.Num(int(s, 0)))
+                s = ''
+                t = None
+        elif t == 'l': # label or reg
+            if c.isalnum() or c == '_':
+                s += c
+            else:
+                domore = True
+                if asm.Reg.is_reg(s):
+                    a = asm.Reg(s)
+                else:
+                    a = asm.Label(s)
+                args.append(a)
+                s = ''
+                t = None
+        else:
+            raise ValueError("Internal error: illegal type state %s" % t)
+
+        if domore: # current character was not processed yet
+            if c.isdigit():
+                s += c
+                t = 'n'
+            elif c.isalpha() or c == '_':
+                s += c
+                t = 'l' # label
+            elif c in "'\"": # quoted str
+                t = c
+            elif c.isspace(): # including last \n
+                continue # skip
+            elif c == ',':
+                continue # skip - is it a good approach? allows both "MOV R0,R1" and "MOV R1 R1"
+            elif c == '[':
+                if br:
+                    raise SyntaxError("Nested [] are not supported", pos)
+                br = True
+                gargs = args
+                args = asm.List()
+            elif c == ']':
+                if not br:
+                    raise SyntaxError("Unmatched ]", f, line)
+                gargs.append(args)
+                args = gargs
+                br = False
+            else:
+                raise SyntaxError("Bad character: %c" % c, pos)
+    # now let's check that everything went clean
+    if t:
+        raise SyntaxError("Unterminated string? %c" % t, pos)
+    if br:
+        raise SyntaxError("Unmatched '['", pos)
+
+    # FIXME: convert and return ready instruction
+    return opcode, args
+
 def parseAsm(f, prev, pos):
     """
     Usage: pass it file after you encounter { (block beginning)
@@ -62,7 +152,6 @@ def parseAsm(f, prev, pos):
     for lnum, line in enumerate(chain(prev, f), pos.getLnum()+1-len(prev)):
         line = uncomment(line)
         pos.setLine(lnum, line)
-
         # ignore empty lines
         if not line:
             continue
@@ -75,91 +164,9 @@ def parseAsm(f, prev, pos):
                 print "Warning: spare characters after '}', will ignore"
             break
 
-        try:
-            opcode, arg = line.split(None,1)
-        except ValueError: # only one token
-            opcode = line
-            arg = ''
+        instr = parseInstruction(line, pos)
 
-        # now parse args
-        args = []
-        s = '' # string repr of current arg
-        t = None # type of current arg: None (no current), n(numeric), ',"(quoted str), l(reg or label)
-        br = False # whether we are in [] block
-        for c in arg+'\n': # \n will be processed as last character
-            domore = False # if current character needs to be processed further
-            if t == None: # state: no current arg
-                domore = True
-            elif t in "'\"": # quoted string
-                if c == t: # end of string
-                    args.append(asm.Str(s))
-                    s = ''
-                    t = None
-                elif c == '\\': # backslash in string
-                    t += '\\'
-                else:
-                    s += c
-            elif t in ['"\\', "'\\"]: # state: backslash in quoted string
-                s += c
-                t=t[0]
-            elif t == 'n': # number, maybe hex
-                if c.isdigit() or c in 'xXbB':
-                    s.append(c)
-                else:
-                    domore = True # need to process current character further
-                    args.append(asm.Num(int(s, 0)))
-                    s = ''
-                    t = None
-            elif t == 'l': # label or reg
-                if c.isalnum() or c == '_':
-                    s += c
-                else:
-                    domore = True
-                    if asm.Reg.is_reg(s):
-                        a = asm.Reg(s)
-                    else:
-                        a = asm.Label(s)
-                    args.append(a)
-                    s = ''
-                    t = None
-            else:
-                raise ValueError("Internal error: illegal type state %s" % t)
-
-            if domore: # current character was not processed yet
-                if c.isdigit():
-                    s += c
-                    t = 'n'
-                elif c.isalpha() or c == '_':
-                    s += c
-                    t = 'l' # label
-                elif c in "'\"": # quoted str
-                    t = c
-                elif c.isspace(): # including last \n
-                    continue # skip
-                elif c == ',':
-                    continue # skip - is it a good approach? allows both "MOV R0,R1" and "MOV R1 R1"
-                elif c == '[':
-                    if br:
-                        raise SyntaxError("Nested [] are not supported", pos)
-                    br = True
-                    gargs = args
-                    args = asm.List()
-                elif c == ']':
-                    if not br:
-                        raise SyntaxError("Unmatched ]", f, line)
-                    gargs.append(args)
-                    args = gargs
-                    br = False
-                else:
-                    raise SyntaxError("Bad character: %c" % c, pos)
-        # now let's check that everything went clean
-        if t:
-            raise SyntaxError("Unterminated string? %c" % t, pos)
-        if br:
-            raise SyntaxError("Unmatched '['", pos)
-
-        # now we have a good instruction
-        instructions.append((opcode, args))
+        instructions.append(instr)
     return instructions
 
 def parseBlock(f, pos):
