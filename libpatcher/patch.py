9fe59dc382042384159e7b55327f43028a64a922
Semyon Maryasin <simeon@maryasin.name>
2014-04-14 23:34:52 +0400

    patch: added context and library properties

--- libpatcher/patch.py
+++ libpatcher/patch.py
@@ -5,12 +5,28 @@ class Patch(object):
     with all its blocks and its global context.
     It may also represent aggregation of #included ("library") patchfiles.
     """
-    def __init__(self, name, blocks=[]):
+    def __init__(self, name, library=None, blocks=[]):
         self.name = name
         self.blocks = blocks
+        self._library = library
         self._is_bound = False
+        self._context = {}
     def __repr__(self):
         return "<patch:%s, %s blocks>" % (self.name, len(self.blocks))
+    @property
+    def library(self):
+        """
+        This links to a library patch, which holds
+        all "included" patches' data.
+        It may link to itself.
+        """
+        return self._library
+    @property
+    def context(self):
+        """
+        This is a patch-level global context.
+        """
+        return self._context
     def bindall(self, binary, codebase = 0x8010000):
         """
         Tries to bind all blocks of this patch
@@ -25,6 +41,7 @@ class Patch(object):
         """
         Applies all blocks from this patch to given binary,
         and returns resulting patched binary.
+        Will bind itself firstly if neccessary.
         """
         if not self._is_bound:
             self.bindall(binary, codebase)
