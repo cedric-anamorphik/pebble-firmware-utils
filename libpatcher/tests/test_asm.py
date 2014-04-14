f41709517b5af8c3b0fe09f49758b46eac0ad755
Semyon Maryasin <simeon@maryasin.name>
2014-04-14 18:53:12 +0400

    tests/asm: added new tests

--- libpatcher/tests/test_asm.py
+++ libpatcher/tests/test_asm.py
@@ -1,8 +1,8 @@
 from libpatcher.asm import *
-from libpatcher.parser import FilePos, parseInstruction
+from libpatcher.parser import FilePos, parseBlock, parseInstruction
 from nose.tools import *
 
-def op(instr, addr=0):
+def op(instr, addr=0, context={}):
     pos = FilePos('test_asm.pbp',0)
     if type(instr) is str:
         i = parseInstruction(instr, pos)
@@ -12,14 +12,25 @@ def op(instr, addr=0):
     if addr < 0x8010000:
         addr += 0x8010000
     i.setAddr(addr)
+    block = [i]
+    #FIXME: block.context = context
+    i.setBlock(block)
     return i.getCode()
 
-@istest
-def ADD_R1_1():
+def test_ADD_R1_1():
     assert op(('ADD', [Reg('R1'), Num(1)])) == '\x01\x31'
-#@istest
-def ADD_R3_R0_R2():
+@nottest
+def test_ADD_R3_R0_R2():
     assert op('ADD R3,R0,R2') == '\x83\x18'
-#@istest
-def MOV_R0_2C():
+@nottest
+def test_MOV_R0_2C():
     assert op('MOV R0,0x2C') == '\x2c\x20'
+@nottest
+def test_BL_self():
+    code = "label: BL label"
+    assert False, "Not implemented yet"
+def test_DCH_0x1234():
+    assert op('DCH 0x1234') == '\x34\x12'
+@raises(SyntaxError)
+def test_DCH_too_large():
+    op('DCH 0x12345')
