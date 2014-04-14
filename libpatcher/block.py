a789b278e2f37278d375b8bddd0390acf43687fb
Semyon Maryasin <simeon@maryasin.name>
2014-04-14 23:13:14 +0400

    block: added getMask, getPosition

--- libpatcher/block.py
+++ libpatcher/block.py
@@ -4,11 +4,28 @@ class Block(object):
         self.mask = mask
         self.instructions = instructions
         self.context = {}
+        self.position = None # to cache mask.match() result
     def __repr__(self):
         return "<<<Block at\n%s:\n%s\n>>>" % (repr(self.mask), '\n'.join([repr(x) for x in self.instructions]))
     def getContext(self):
         " Returns block-local context dictionary "
         return self.context
+    def getMask(self):
+        return self.mask
+    def getPosition(self, binary=None):
+        """
+        Returns position of this block's mask in given binary file.
+        Will cache its result.
+        """
+        if self.position == None \
+                or (binary and self._binary != binary):
+            # if position was not calculated yet
+            # or if bin/codebase provided and not same as were saved
+            if binary is None:
+                raise ValueError("No position nor binary provided")
+            self.position = self.mask.match(binary) + codebase
+            self._binary, self._codebase = binary, codebase
+        return self.position
     def bind(self, addr):
         """
         This method is called once after construction.
