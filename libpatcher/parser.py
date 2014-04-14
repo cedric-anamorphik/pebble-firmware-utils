7fe7cc7790e6b8f70ee136e53888a9f0de8d7f9c
Semyon Maryasin <simeon@maryasin.name>
2014-04-15 03:36:53 +0400

    parser: fixed support for db HEX

--- libpatcher/parser.py
+++ libpatcher/parser.py
@@ -113,7 +113,7 @@ def parseInstruction(line, pos):
             raise ValueError("Internal error: illegal type state %s" % t)
 
         if domore: # current character was not processed yet
-            if c.isdigit() or c == '-':
+            if c.isdigit() or c == '-' or (opcode == "db" and c in "AaBbCcDdEeFf"):
                 s += c
                 t = 'n'
             elif c.isalpha() or c == '_':
