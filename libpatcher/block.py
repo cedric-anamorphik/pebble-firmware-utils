23a5e41e5ec7aaa04419b7fa95acfc257cda32fd
Semyon Maryasin <simeon@maryasin.name>
2014-04-14 22:45:56 +0400

    Revert "block: added Block.apply (wip, unneeded)"
    This reverts commit 5753d1848579207ca5b9c311b5025373b9b751ec.

--- libpatcher/block.py
+++ libpatcher/block.py
@@ -14,18 +14,8 @@ class Block(object):
         This method is called once after construction.
         It binds block to specific memory address (which is determined with mask.match)
         """
-        if self.addr:
-            raise ValueError("Mask was already bound to %s" + self.addr)
         self.addr = addr
         for i in self.instructions:
             i.setAddr(addr)
             addr += i.getSize()
             i.setBlock(self) # it may in return update our context, so call after setAddr
-    def apply(self, binary):
-        """
-        Applies this block to given binary
-        and returns resulting binary.
-        Can raise MaskNotFound / AmbiguousMask
-        """
-        pos = self.mask.match(binary)
-        self.bind(pos + 0x8010000) # convert pos to mem addr
