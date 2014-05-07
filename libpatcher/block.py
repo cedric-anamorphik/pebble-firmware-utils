6ddddee35a2f8424468cdfa0709c84c8f48d7000
Semyon Maryasin <simeon@maryasin.name>
2014-05-08 01:18:49 +0400

    block: removed binary checking; fixed bug with floating mask

--- libpatcher/block.py
+++ libpatcher/block.py
@@ -37,19 +37,16 @@ class Block(object):
         Returns position of this block's mask in given binary file.
         Will cache its result.
         """
-        if self.position == None \
-                or (binary and self._binary != binary):
+        if self.position == None:
             # if position was not calculated yet
-            # or if bin/codebase provided and not same as were saved
             if self.mask.floating:
                 if ranges == None:
                     raise ValueError("No ranges provided for floating block")
-                self.position = ranges.find(self.getSize())
+                self.position = ranges.find(self.getSize())[0]
             else:
                 if binary is None:
                     raise ValueError("No saved position and binary not provided")
                 self.position = self.mask.match(binary)
-                self._binary = binary
         return self.position
     def bind(self, addr):
         """
