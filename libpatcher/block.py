e72255c2567e1da0747552026554b77b5be30e3e
Semyon Maryasin <simeon@maryasin.name>
2014-05-08 00:49:17 +0400

    block,mask: implemented "floating masks"
    This allows to avoid special handling of empty masks in parser

--- libpatcher/block.py
+++ libpatcher/block.py
@@ -41,15 +41,15 @@ class Block(object):
                 or (binary and self._binary != binary):
             # if position was not calculated yet
             # or if bin/codebase provided and not same as were saved
-            if self.mask: # fixed block
+            if self.mask.floating:
+                if ranges == None:
+                    raise ValueError("No ranges provided for floating block")
+                self.position = ranges.find(self.getSize())
+            else:
                 if binary is None:
                     raise ValueError("No saved position and binary not provided")
                 self.position = self.mask.match(binary)
                 self._binary = binary
-            else: # floating block
-                if ranges == None:
-                    raise ValueError("No ranges provided for floating block")
-                self.position = ranges.find(self.getSize())
         return self.position
     def bind(self, addr):
         """
--- libpatcher/mask.py
+++ libpatcher/mask.py
@@ -18,12 +18,17 @@ class Mask(object):
         self.pos = pos
         # TODO: validate
     def __repr__(self):
+        if not self.parts: # floating mask
+            return "Floating mask"
         def str2hex(s):
             if type(s) is int:
                 return "?%d" % s
             else:
                 return ' '.join(["%02X" % ord(c) for c in s])
         return "Mask at %s: %s @%d" % (self.pos, ','.join([str2hex(x) for x in self.parts]), self.offset)
+    @property
+    def floating(self):
+        return not self.parts or len(self.parts) == 0
     def match(self, data):
         """
         Tries to match this mask to given data.
@@ -31,6 +36,8 @@ class Mask(object):
         False if not found
         or (exception?) if found more than one occurance.
         """
+        if self.floating:
+            raise ValueError("Cannot match floating mask")
         # if mask starts with skip, append it to offset
         if type(self.parts[0]) is int:
             self.offset += self.parts[0]
