bb4c2c929abdd1948f8ede1362e3657843dbedee
Semyon Maryasin <simeon@maryasin.name>
2015-03-08 21:57:56 +0300

    getver: allow downloading only some hw versions

--- getver.sh
+++ getver.sh
@@ -7,15 +7,18 @@
 ver=$1
 short=$2
 channel=${3:-release-v2}
+channel=release-v2
 [[ $ver == *beta* ]] && channel=beta
 if [ -z "$short" ]; then
-	echo "Usage: $0 version shorthand [channel]"
+	echo "Usage: $0 version shorthand ['hw_versions']"
 	echo "Example: $0 2.2 v220"
 	echo "Example: $0 2.9-beta5 v29b5     # channel will be automatigically set to beta"
 	exit 1
 fi
 
-for hw in ev2_4 v1_5 v2_0; do
+hardwares=${3:-ev2_4 v1_5 v2_0}
+
+for hw in $hardwares; do
 	echo "Downloading version $ver for HW $hw"
 	mkdir $hw$short
 	cd $hw$short
