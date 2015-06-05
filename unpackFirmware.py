f0f549b69c607013ba5a6f8fe89374d978797d05
Semyon Maryasin <simeon@maryasin.name>
2015-06-05 13:47:00 +0300

    unpackFirmware: 2to3

--- unpackFirmware.py
+++ unpackFirmware.py
@@ -14,48 +14,48 @@ def mkdir(path):
 		pass
 
 def extract_content(pbz, content, output_dir):
-	print 'Extracting %s...' % content['name']
+	print('Extracting %s...' % content['name'])
 	pbz.extract(content['name'], output_dir)
 	data = io.FileIO(output_dir + content['name']).readall()
 	crc = crc32(data)
 	if crc == content['crc']:
-		print '\t[  OK] Checking CRC...'
+		print('\t[  OK] Checking CRC...')
 	else:
-		print '\t[Fail] Checking CRC...'
-		print "\tIt's %d, but should be %d" % (content['crc'], crc)
+		print('\t[Fail] Checking CRC...')
+		print("\tIt's %d, but should be %d" % (content['crc'], crc))
 
 def extract_resources(pbpack, resourceMap, output_dir):
 	numbers = unpack('B', pbpack.read(1))[0]
-	print 'Resource pack has %d resources.' % numbers
+	print('Resource pack has %d resources.' % numbers)
 
 	pbpack.seek(4)
 	crc_from_json = unpack('I', pbpack.read(4))[0]
-	print 'Resource pack claims to have crc 0x%X.' % crc_from_json
+	print('Resource pack claims to have crc 0x%X.' % crc_from_json)
 
 	offset=None # this will be set depending on firmware version
 
-	print 'Checking CRC with offset 0x100C (2.x)...'
+	print('Checking CRC with offset 0x100C (2.x)...')
 	pbpack.seek(0x100C)
 	crc_resource = crc32(pbpack.read())
 	if crc_resource == crc_from_json:
-		print '\t[  OK] This seems to be a 2.x firmware.'
+		print('\t[  OK] This seems to be a 2.x firmware.')
 		offset = 0x0C
 	else:
-		print '\t[Info] It is not 2.x firmware, or is corrupted, CRC=0x%08X' % crc_resource
-		print 'Checking CRC with offset 0x101C (1.x)...'
+		print('\t[Info] It is not 2.x firmware, or is corrupted, CRC=0x%08X' % crc_resource)
+		print('Checking CRC with offset 0x101C (1.x)...')
 		pbpack.seek(0x101C)
 		crc_resource2 = crc32(pbpack.read())
 		if crc_resource2 == crc_from_json:
-			print '\t[  OK] This seems to be a 1.x firmware.'
+			print('\t[  OK] This seems to be a 1.x firmware.')
 			offset = 0x1C
 		else:
-			print '\t[Fail] CRC check failed!'
-			print "\tShould be 0x%X, but if 0x%X for 100c and 0x%x for 101c." % (
-					crc_from_json, crc_resource, crc_resource2)
-			print 'Resource pack is either malformed or has unknown format.'
+			print('\t[Fail] CRC check failed!')
+			print("\tShould be 0x%X, but if 0x%X for 100c and 0x%x for 101c." % (
+					crc_from_json, crc_resource, crc_resource2))
+			print('Resource pack is either malformed or has unknown format.')
 			return
 
-	print 'Reading resurce headers...'
+	print('Reading resurce headers...')
 	resources = {}
 	for i in range(numbers):
 		pbpack.seek(offset + i * 16)
@@ -73,7 +73,7 @@ def extract_resources(pbpack, resourceMap, output_dir):
 		dirname = os.path.dirname(path)
 		filepath = "/".join((dirname, resourceMap[i]['defName'])) if hasRM else path
 
-		print 'Extracting %s...' % filepath
+		print('Extracting %s...' % filepath)
 		mkdir(output_dir + dirname)
 
 		pbpack.seek(0x1000 + offset + entry['offset'])
@@ -84,11 +84,11 @@ def extract_resources(pbpack, resourceMap, output_dir):
 		data = io.FileIO(output_dir + filepath).readall()
 		crc = crc32(data)
 		if crc == entry['crc']:
-			print '\t[  OK] Checking CRC...'
+			print('\t[  OK] Checking CRC...')
 		else:
-			print '\t[Fail] Checking CRC...'
-			print "\tIt's 0x%x, but should be 0x%x" % (crc, entry['crc'])
-	print 'All resources unpacked.'
+			print('\t[Fail] Checking CRC...')
+			print("\tIt's 0x%x, but should be 0x%x" % (crc, entry['crc']))
+	print('All resources unpacked.')
 
 
 if __name__ == '__main__':
@@ -98,16 +98,16 @@ if __name__ == '__main__':
 		sys.argv.pop(1)
 
 	if len(sys.argv) <= 1:
-		print 'Usage: unpackFirmware.py [-i] normal.pbz [output_dir/]'
-		print '	-i: ignore resource filenames from manifest.'
-		print '	   By default, if manifest contains resource names'
-		print '	   (as in 1.x firmwares), we will name resources'
-		print '	   according to them.'
-		print '	   With this option, resource names will be res/ID_CRC'
-		print '	   where ID is sequence index of resource and CRC is'
-		print '	   its CRC checksum (for comparing)'
-		print '	   On 2.x firmwares, which don\'t contain resource names,'
-		print '	   we always use that scheme.'
+		print('Usage: unpackFirmware.py [-i] normal.pbz [output_dir/]')
+		print('	-i: ignore resource filenames from manifest.')
+		print('	   By default, if manifest contains resource names')
+		print('	   (as in 1.x firmwares), we will name resources')
+		print('	   according to them.')
+		print('	   With this option, resource names will be res/ID_CRC')
+		print('	   where ID is sequence index of resource and CRC is')
+		print('	   its CRC checksum (for comparing)')
+		print('	   On 2.x firmwares, which don\'t contain resource names,')
+		print('	   we always use that scheme.')
 		exit()
 
 	pbz_name = sys.argv[1]
@@ -122,13 +122,13 @@ if __name__ == '__main__':
 		output_dir = sys.argv[2]
 		if not output_dir.endswith('/'):
 			output_dir += '/'
-	print 'Will unpack firmware from %s to directory %s, using %s for filenames' % (
+	print('Will unpack firmware from %s to directory %s, using %s for filenames' % (
 			pbz_name, output_dir,
-			'resource names (if possible)' if useNaming else 'resource indices')
+			'resource names (if possible)' if useNaming else 'resource indices'))
 
 	pbz = zipfile.ZipFile(pbz_name)
 
-	print 'Extracting manifest.json...'
+	print('Extracting manifest.json...')
 	pbz.extract('manifest.json', output_dir)
 	manifest = json.load(open(output_dir + 'manifest.json', 'rb'))
 
@@ -141,15 +141,15 @@ if __name__ == '__main__':
 
 		if 'resourceMap' in manifest['debug']:
 			resourceMap = manifest['debug']['resourceMap']['media']
-			print 'Found resource map in manifest. Looks like 1.x firmware.'
+			print('Found resource map in manifest. Looks like 1.x firmware.')
 			if useNaming:
-				print 'Will use it to name resources correctly. To ignore, pass -i option.'
+				print('Will use it to name resources correctly. To ignore, pass -i option.')
 			else:
-				print 'Will not use it however because of -i option'
+				print('Will not use it however because of -i option')
 		else:
 			resourceMap = None
-			print "Couldn't find resource map in manifest. Looks like 2.x firmware."
-			print 'Resources will be named by their indices.'
+			print("Couldn't find resource map in manifest. Looks like 2.x firmware.")
+			print('Resources will be named by their indices.')
 			useNaming = False
 		pbpack = open(output_dir + resources['name'], 'rb')
 		extract_resources(pbpack, resourceMap if useNaming else None, output_dir)
