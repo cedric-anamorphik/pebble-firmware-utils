257abc261954746cc92d5d9324e03d09c158be23
Semyon Maryasin <simeon@maryasin.name>
2015-06-27 01:50:53 +0300

    downloadFirmware: use python2, tabs, added v3 fw support

--- downloadFirmware.py
+++ downloadFirmware.py
@@ -1,27 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # URI = 'http://pebble-static.s3.amazonaws.com/watchfaces/index.html'
-URI = 'http://pebblefw.s3.amazonaws.com/pebble/ev2_4/release/latest.json'
-URI_v2 = 'http://pebblefw.s3.amazonaws.com/pebble/ev2_4/release-v2/latest.json'
+URIs = {
+    1: 'http://pebblefw.s3.amazonaws.com/pebble/ev2_4/release/latest.json',
+    2: 'http://pebblefw.s3.amazonaws.com/pebble/ev2_4/release-v2/latest.json',
+    3: 'http://pebblefw.s3.amazonaws.com/pebble/snowy_dvt/release-v3/latest.json',
+}
 
+import argparse
 from urllib2 import urlopen
 import hashlib
 import logging, os.path
 import json
 
+def parse_args():
+    parser = argparse.ArgumentParser('Download latest firmware bundle from Pebble')
+    parser.add_argument('version', default=3, nargs='?',
+                        choices = URIs,
+                        help='Which version group to use.')
+    return parser.parse_args()
+
 if __name__ == "__main__":
     log = logging.getLogger()
     logging.basicConfig(format='[%(levelname)-8s] %(message)s')
     log.setLevel(logging.DEBUG)
-    
-    log.info("Downloading firmware linked from %s" % URI_v2)
-    
-    page = urlopen(URI_v2).read()
+
+    args = parse_args()
+
+    log.info("Downloading firmware linked from %s" % URIs[args.version])
+
+    page = urlopen(URIs[args.version]).read()
     data = json.loads(page)
-    
+
     firmware = data['normal']['url']
     version  = data['normal']['friendlyVersion']
     sha      = data['normal']['sha-256']
-    
+
     log.info("Latest firmware version: %s" % version)
     fwfile = firmware[firmware.rindex("/")+1:]
     if os.path.exists(fwfile):
@@ -37,13 +50,13 @@ if __name__ == "__main__":
         while downloaded < length:
             data = download.read(1024*50)
             f.write(data)
-            downloaded += len(data) 
+            downloaded += len(data)
             log.info("Downloaded %.1f%%" % (downloaded * 100.0 / length))
-    
+
     f = open(fwfile, "rb")
     filesha = hashlib.sha256()
     filesha.update(f.read())
     if(filesha.hexdigest() != sha):
         log.error('File download errer: SHA-256 hash mismatch. Please retry.')
     else:
-		log.info('File download done.')
+        log.info('File download done.')
