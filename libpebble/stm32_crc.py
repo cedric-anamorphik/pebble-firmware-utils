246f4f91fa8b07e15c2a2eb6e1b8445406ebfef3
Semyon Maryasin <simeon@maryasin.name>
2015-07-01 15:34:19 +0300

    adapted stm32_crc for py3

--- libpebble/stm32_crc.py
+++ libpebble/stm32_crc.py
@@ -14,7 +14,7 @@ def process_word(data, crc=0xffffffff):
     d = array.array('I', data)[0]
     crc = crc ^ d
 
-    for i in xrange(0, 32):
+    for i in range(0, 32):
         if (crc & 0x80000000) != 0:
             crc = (crc << 1) ^ CRC_POLY
         else:
@@ -24,12 +24,12 @@ def process_word(data, crc=0xffffffff):
     return result
 
 def process_buffer(buf, c = 0xffffffff):
-    word_count = len(buf) / 4
+    word_count = len(buf) // 4
     if (len(buf) % 4 != 0):
         word_count += 1
 
     crc = c
-    for i in xrange(0, word_count):
+    for i in range(0, word_count):
         crc = process_word(buf[i * 4 : (i + 1) * 4], crc)
     return crc
 
