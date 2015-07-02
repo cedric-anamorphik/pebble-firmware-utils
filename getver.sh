bab8f9d678bc2a09a5439b32342315cdbfa01a1b
Semyon Maryasin <simeon@maryasin.name>
2015-07-02 16:44:01 +0300

    getver: updated to use v3 by default

--- getver.sh
+++ getver.sh
@@ -6,22 +6,22 @@
 
 ver=$1
 short=$2
-channel=${3:-release-v2}
-channel=release-v2
+channel=${4:-release-v3}
 [[ $ver == *beta* ]] && channel=beta
 if [ -z "$short" ]; then
-	echo "Usage: $0 version shorthand ['hw_versions']"
+	echo "Usage: $0 version shorthand ['hw_versions' [platform]"
 	echo "Example: $0 2.2 v220"
 	echo "Example: $0 2.9-beta5 v29b5     # channel will be automatigically set to beta"
 	exit 1
 fi
 
-hardwares=${3:-ev2_4 v1_5 v2_0}
+#hardwares=${3:-ev2_4 v1_5 v2_0}
+hardwares=${3:-snowy_dvt}
 
 for hw in $hardwares; do
 	echo "Downloading version $ver for HW $hw"
-	mkdir $hw$short
-	cd $hw$short
+	mkdir $short-$hw
+	cd $short-$hw
 	outfile="Pebble-$ver-${hw}.pbz"
 	[ -e "$outfile" ] && rm "$outfile"
 	wget "https://pebblefw.s3.amazonaws.com/pebble/$hw/$channel/pbz/Pebble-$ver-$hw.pbz"
