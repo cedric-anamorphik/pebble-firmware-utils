ed092df0ad35db6c6497f3d837027bbc6c926054
Semyon Maryasin <simeon@maryasin.name>
2015-08-27 00:54:17 +0300

    Updated getver.sh for pebble time steel (s3)

--- getver.sh
+++ getver.sh
@@ -16,7 +16,7 @@ if [ -z "$short" ]; then
 fi
 
 #hardwares=${3:-ev2_4 v1_5 v2_0}
-hardwares=${3:-snowy_dvt}
+hardwares=${3:-snowy_dvt snowy_s3}
 
 for hw in $hardwares; do
 	echo "Downloading version $ver for HW $hw"
