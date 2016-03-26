e797730be34ec12e61fd305142bb8efaf6164ea4
Semyon Maryasin <simeon@maryasin.name>
2016-03-26 03:42:28 +0300

    findlog fix?

--- findlog.sh
+++ findlog.sh
@@ -4,4 +4,4 @@ if [ -z $1 ]; then
 	exit 1
 fi
 # get last 4 characters
-wcalc 0x${1: -4} | sed 's/^[^0-9]*/\\"/;s/\..*/\\" loghash_dict.json/' | xargs grep
+wcalc 0x${1: -4} | sed 's/^[^0-9]*/\\"/;s/$/\\" loghash_dict.json/' | xargs grep
