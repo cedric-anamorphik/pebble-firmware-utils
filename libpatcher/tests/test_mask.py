d31de2d1a31bab0ae338a2ab49ef8d72efddd8e9
Semyon Maryasin <simeon@maryasin.name>
2016-05-24 14:11:15 +0300

    test_mask: lint

--- libpatcher/tests/test_mask.py
+++ libpatcher/tests/test_mask.py
@@ -1,11 +1,13 @@
-from libpatcher.mask import *
-from nose.tools import *
+from libpatcher.mask import Mask, MaskNotFoundError
+from nose.tools import eq_, raises
 
-ma = Mask(['hello',3,'world'])
+ma = Mask(['hello', 3, 'world'])
 da1 = 'hello!!!world'
 da2 = 'hello_world'
+
 def test_maskA_match():
     eq_(ma.match(da1), 0)
+
 @raises(MaskNotFoundError)
 def test_maskA_not_match():
     ma.match(da2)
