dd4359ae62224aa6050c546b95c49ddccb6f3a70
Semyon Maryasin <simeon@maryasin.name>
2016-06-01 03:27:30 +0300

    pbpack: use temp files

--- pbpack.py
+++ pbpack.py
@@ -5,51 +5,53 @@ import os
 import sys
 import time
 import struct
+from tempfile import TemporaryFile
 
 from libpebble import stm32_crc
 
 MAX_NUM_FILES = 256
 BYTES_PER_TABLE_ENTRY = 16
 
-def manifest(manifest_file_path, data_chunk_file, num_files, timestamp):
-    with open(manifest_file_path, 'wb') as manifest_file:
-        with open(data_chunk_file, 'rb') as data_file:
-            crc = stm32_crc.crc32(data_file.read())
-            manifest_file.write(struct.pack('<III', int(num_files), crc, int(timestamp)))
+def manifest(manifest_file, data_file, num_files, timestamp):
+    data_file.seek(0)  # first rewind it..
+    crc = stm32_crc.crc32(data_file.read())
+    manifest_file.write(struct.pack('<III', int(num_files), crc, int(timestamp)))
 
 def table(table_file, pack_file_list, ):
-    with open(table_file, 'wb') as table_file:
+    cur_file_id = 1
+    next_free_byte = 0
 
-        cur_file_id = 1
-        next_free_byte = 0
+    for filename in pack_file_list:
+        with open(filename, 'rb') as data_file:
+            content = data_file.read()
+            length = len(content)
+            table_file.write(struct.pack('<IIII', cur_file_id, next_free_byte, length, stm32_crc.crc32(content)))
+            cur_file_id += 1
+            next_free_byte += length
 
-        for filename in pack_file_list:
-            with open(filename, 'rb') as data_file:
-                content = data_file.read()
-                length = len(content)
-                table_file.write(struct.pack('<IIII', cur_file_id, next_free_byte, length, stm32_crc.crc32(content)))
-                cur_file_id += 1
-                next_free_byte += length
-
-        # pad the rest of the file
-        for i in range(len(pack_file_list), MAX_NUM_FILES):
-            table_file.write(struct.pack('<IIII', 0, 0, 0, 0))
+    # pad the rest of the file
+    for i in range(len(pack_file_list), MAX_NUM_FILES):
+        table_file.write(struct.pack('<IIII', 0, 0, 0, 0))
 
 def pack(resources, pbfile):
 
     timestamp = int(time.time())
 
-    table(pbfile+'.table', resources)
+    with TemporaryFile('manifest') as tmp_manifest, \
+         TemporaryFile('table') as tmp_table, \
+         TemporaryFile('data') as tmp_data:
 
-    with open(pbfile+'.data', 'wb') as f:
+        table(tmp_table, resources)
+
         for res in resources:
-            f.write(open(res, 'rb').read())
+            tmp_data.write(open(res, 'rb').read())
 
-    manifest(pbfile+'.manifest', pbfile+'.data', len(resources), timestamp)
+        manifest(tmp_manifest, tmp_data, len(resources), timestamp)
 
-    with open(pbfile, 'wb') as f:
-        for t in ('manifest', 'table', 'data'):
-            f.write(open(pbfile+'.'+t, 'rb').read())
+        with open(pbfile, 'wb') as f:
+            for t in (tmp_manifest, tmp_table, tmp_data):
+                t.seek(0)
+                f.write(t.read())
 
 
 if __name__ == '__main__':
