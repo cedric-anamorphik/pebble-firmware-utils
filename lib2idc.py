7ac6b1839060f1d1f3180e8752c929af68d99a26
Semyon Maryasin <simeon@maryasin.name>
2016-07-25 14:42:13 +0300

    2to3 lib2idc

--- lib2idc.py
+++ lib2idc.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import sys
 from struct import unpack
 
 if len(sys.argv) < 5:
-    print "Usage:"
-    print "lib2idc.py libpebble.a (names-offset) (funcs-offset) (pbl_table-offset)"
-    print "for sdk 2.0b3 offsets are: 0x9D8, 0x40AE"
+    print("Usage:")
+    print("lib2idc.py libpebble.a (names-offset) (funcs-offset) (pbl_table-offset)")
+    print("for sdk 2.0b3 offsets are: 0x9D8, 0x40AE")
     sys.exit(1)
 
 szLibfile, szNames, szFuncs, pbl_table = sys.argv[1:]
@@ -31,21 +31,21 @@ funcs = []
 addrs = []
 for i in range(len(names)):
     proc = f.read(12)
-    if len(proc) < 12: # end of file reached
+    if len(proc) < 12:  # end of file reached
         break
     addr = unpack("<LLL", proc)[2]
     funcs.append((addr, names[i]))
     addrs.append(addr)
 
-print "#include <idc.idc>"
-print
-print "static main(void) {"
-print "\tauto pbl_table = %s;" % pbl_table
+print("#include <idc.idc>")
+print()
+print("static main(void) {")
+print("\tauto pbl_table = %s;" % pbl_table)
 for f in funcs:
-    print '\tMakeUnkn(Dword(pbl_table+0x%08X)-1, DOUNK_DELNAMES);' % f[0]
-    print '\tMakeName(Dword(pbl_table+0x%08X)-1, "%s");' % f
-    print '\tMakeFunction(Dword(pbl_table+0x%08X)-1, BADADDR);' % f[0]
-print "\t// Now mark these offsets (somewhat buggy approach though)"
+    print('\tMakeUnkn(Dword(pbl_table+0x%08X)-1, DOUNK_DELNAMES);' % f[0])
+    print('\tMakeName(Dword(pbl_table+0x%08X)-1, "%s");' % f)
+    print('\tMakeFunction(Dword(pbl_table+0x%08X)-1, BADADDR);' % f[0])
+print("\t// Now mark these offsets (somewhat buggy approach though)")
 for i in range(0, len(funcs)*4, 4):
-    print "\tOpOff(pbl_table+0x%08X, 0, 0);" % i
-print "}"
+    print("\tOpOff(pbl_table+0x%08X, 0, 0);" % i)
+print("}")
